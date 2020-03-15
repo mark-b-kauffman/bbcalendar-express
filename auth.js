7017c3fb6b51c0adfe53ae874a673de4f95f1fbc
mark-b-kauffman <mark.kauffman@blackboard.com>
2020-03-15 16:46:37 -0700

    going to /calendars redirects to get the authroization code from the learn server. Next - get the access token

--- auth.js
+++ auth.js
@@ -10,7 +10,7 @@ const url = require('url');
 
 var authobj = {authcode:null, server:null, token:null, user:null }; 
 
-function getAuthCode(req, learnserver, key){
+function getAuthCode(req, res, learnserver, key){
   console.log(`learnserver:${learnserver} key:${key}`);
   // build the redirectUri based on where we are currently running.
   var protocol = req.protocol;
@@ -19,15 +19,39 @@ function getAuthCode(req, learnserver, key){
   var path = url.parse(req.originalUrl).pathname;
   var redirectUri = protocol+"://"+subdomains+hostname+path;
   console.log(`redirectUri: ${redirectUri}`);
-  // let url = `https://${learnserver}/learn/api/public/v1/oauth2/authorizationcode?redirect_uri=http://localhost:3000&scope=*&response_type=code&client_id=${key}`;
+  let authcodeUrl = `https://${learnserver}/learn/api/public/v1/oauth2/authorizationcode?redirect_uri=${redirectUri}&scope=*&response_type=code&client_id=${key}`;
+  console.log(`authcodeUrl:${authcodeUrl}`)
+  authobj.authcode = req.query.code;
   if (authobj.authcode == null) {
     console.log('authcode was null');
-    authobj.authcode = 1;
+    res.redirect(authcodeUrl);
   } else {
-    authobj.authcode = authobj.authcode + 1;
+    authobj.authcode = authobj.authcode;
   }
-}
+} // END getAuthCode
+
+// This should only be called AFTER we have a authorization code.
+function getToken(req, res, learnserver, key, secret){
+  let tokenUrl = `https://${learnserver}/learn/ap/public/v1/oauth2/token?code=${authobj.authcode}`;
+  console.log(`tokenUrl:${tokenUrl}`);
+  var authHeader = 'Basic ' + new Buffer(key + ':' + secret).toString('base64');
+  var options = {                
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded body',
+                Authorization: authHeader
+              },
+    form: {grant_type: 'authorization_code'},
+    strictSSL: true,
+    resolveWithFullResponse: false,
+    json: true
+  }; 
+  //let resp = request('POST', tokenUrl, options);
+  //let respBody = resp.getBody();
+  //console.log(`resp.getBody:${respBody}`);   
+} //END getToken
+
+
 module.exports = {
   authobj,
   getAuthCode,
+  getToken,
 }
--- routes/calendar.js
+++ routes/calendar.js
@@ -1,6 +1,7 @@
 var auth = require('../auth');
 var authobj = auth.authobj;
 var getAuthCode = auth.getAuthCode;
+var getToken = auth.getToken;
 
 var constants = require('../constants');
 var express = require('express');
@@ -10,11 +11,18 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   if (authobj.authcode === null){
     // got no authorization code. need to get one.
-    getAuthCode(req, constants.LEARNSERVER, constants.KEY);
+    // In this first case we redirect to get the code on the query string.
+    getAuthCode(req, res, constants.LEARNSERVER, constants.KEY);
   } else {
-    auth.getAuthCode(req, constants.LEARNSERVER, constants.KEY);
+    // This is REQUIRED. In this second case we pull the code from the query string.
+    getAuthCode(req, res, constants.LEARNSERVER, constants.KEY);
   }
-  res.send(`todo: authcode ${authobj.authcode}, accesstoken, reuqest users calendars`);
+
+  if (authobj.authcode != null && authobj.token == null){
+    getToken(req,res, constants.LEARNSERVER, constants.KEY, constants.SECRET);
+  }
+
+  res.send(`todo: authcode ${authobj.authcode}, accesstoken, request users calendars`);
 });
 
 module.exports = router;
